08dddcae190cc4445f20c9f619fae42f7a67ae75
frioss_ <48369676+exfrioss@users.noreply.github.com>
2021-07-20 11:16:46 -0400

    Explorer python your own: Add some more quote; print out more than one quote; Remove that extra line; add quotes programatically

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,19 @@
 import random
 def primary():
   print("Keep it logically awesome.")
-
+  input_quotes = input("Enter yours quotes please!")
+  f = open("quotes.txt", "a")
+  f.write(input_quotes)
+  f.close()
   f = open("quotes.txt")
   quotes = f.readlines()
   last = len(quotes) -1
   rnd = random.randint(0, last)
+  rnd1 = random.randint(0, last)
   f.close()
 
-  print(quotes[rnd])
+  print(quotes[rnd], end="")
+  print(quotes[rnd1], end="")
 
 if __name__== "__main__":
   primary()
